7644b4401e20fa22c3d08488180cfe673af2414d
Adam Pryka <wishmaster80@o2.pl>
2016-03-22 16:56:23 +0100

    win logic

--- Task2/tic-tac-toe.js
+++ Task2/tic-tac-toe.js
@@ -5,7 +5,8 @@ function AddClass(obj, className) {
     
 }
 function ContainsClass(obj, className) {
-    if (obj.className.indexOf(className) > 0) {
+    //console.log(obj.className + "|" + classname + "|" + obj.className.indexOf(className));
+    if (obj.className.indexOf(className) >= 0) {
         return true;
     }
     return false;
@@ -28,52 +29,65 @@ function ChangeNextTurn() {
     }
 }
 var wining = [
-["top left", "top middle", "top right"]
+                ["square top left", "square top middle", "square top right"],
+                ["square left", "square middle", "square right"],
+                ["square bottom left", "square bottom middle", "square bottom right"],
 
-];
+                ["square top left", "square middle", "square bottom right"],
+                ["square bottom left", "square middle", "square top right"],                
 
-function ChceckPlayer(clicked) {    
+                ["square top left", "square left", "square bottom left"],
+                ["square top middle", "square middle", "square bottom middle"],
+                ["square top right", "square right", "square bottom right"],
+            ];
+
+function ChceckPlayer(clicked) {
+    var date1 = new Date().getTime();
     for (var j = 0; j < wining.length; j++) {        
+        var hits = 0;
         for (var k = 0; k < wining[j].length; k++) {            
-            for (var i = 0; i < clicked.length; i++) {
-                if (!ContainsClass(clicked[i], wining[j][k])) {
-                    
-                    break;
-                }
-                //alert("WIN");
+            for (var i = 0; i < clicked.length; i++) {                
+                console.log(i + " :" + clicked[i].className + " hits:" + hits + wining[j][k] + ":" + clicked[i].className.indexOf(wining[j][k]));
+                if (ContainsClass(clicked[i], wining[j][k])) {                
+                    hits = hits +1;                    
+                }                
+            }
+            if (hits == 3) {                
+                alert("WIN");
             }
         }
-    }
+    }    
 }
 function CheckScore() {
+    console.log("CheckScore");
     var x = [];
     var o = [];
-    for (var i = 0; i < classname.length; i++) {
-        var square = classname[i];
+    for (var i = 0; i < classname.length; i++) {        
+        var square = classname[i];        
         if(ContainsClass(square, "oGreen")){
             o.push(square);
         }
         if (ContainsClass(square, "xGreen")) {
             x.push(square);
         }
-        ChceckPlayer(x);
-        ChceckPlayer(o);
-    }    
+    }
+    ChceckPlayer(x);
+    ChceckPlayer(o);
 }
 
 
 var myFunction = function () {        
     if (IsNotBlank(this) == true)
-        return;
-
+        return;    
     if (nextTurn == 'X') {
         AddClass(this, "xGreen");
         
     } else {
         AddClass(this, "oGreen");        
     }
-    CheckScore();
+    console.log("myFunctiopn");
     ChangeNextTurn();
+    CheckScore();
 };
 
 
