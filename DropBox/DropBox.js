8c3c1e8ccfa88eeb1e511b2ace0d26077ef6cb45
Adam Pryka <wishmaster80@o2.pl>
2016-04-18 19:40:53 +0200

    dropbox

---
+++ DropBox/DropBox.js
@@ -0,0 +1,36 @@
+﻿function Authorize()
+{
+    return true;
+}
+
+
+function Submit()
+{
+    var client = new Dropbox.Client({ key: "beZcac5_WFAAAAAAAAAAF4F2o8-Q1RqU6ZiFKYnrEpgeETTTbFhpdqBuyWjf_aNV" });
+    if (client.isAuthenticated()) { console.log(":)"); }
+    else {
+        console.log(":(");
+    }
+    
+    //Authorize().then(function (ret) {
+    //    alert(ret);
+    //});
+}
+
+function readFile(filename, enc) {
+    return new Promise(function (fulfill, reject) {
+        fs.readFile(filename, enc, function (err, res) {
+            if (err) reject(err);
+            else fulfill(res);
+        });
+    });
+}
+
+//App key
+
+//r4vdwcjpvu6hn1y
+//App secret
+
+//t9t0ujo6p0d3b5s
+
+//beZcac5_WFAAAAAAAAAAF4F2o8-Q1RqU6ZiFKYnrEpgeETTTbFhpdqBuyWjf_aNV
