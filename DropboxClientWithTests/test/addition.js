ed6b93bcef0e44621370dad289d978f465ff3459
Adam Pryka <adam.pryka@gmail.com>
2016-06-20 19:51:40 +0200

    not working :(

--- DropboxClientWithTests/test/addition.js
+++ DropboxClientWithTests/test/addition.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var myFunc1 = require('../app/dropbox-datastores-1.2.0.js');
+var myFunc2 = require('../app/DropBox.js');
+
+
 
 describe('addition', function () {
   it('should add 1+1 correctly', function (done) {
@@ -13,11 +17,21 @@ describe('addition', function () {
 
 
 describe('addition', function () {
-  it('should add 1+1 correctly', function (done) {
+  it('should add 1+3 correctly', function (done) {
     var onePlusOne = 1 + 3;
     onePlusOne.should.equal(4);
     // must call done() so that mocha know that we are... done.
     // Useful for async tests.
+    done();
+  });
+});
+
+
+
+
+describe('addition', function () {
+  it('test1', function (done) {
+
     done();
   });
 });
